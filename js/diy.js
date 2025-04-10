455a70aa219cad3085a085df7eaf2d6b614a0fdd
lyh007light <1558395249@qq.com>
2025-04-10 22:58:34 +0800

    Add files via upload

---
+++ js/diy.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'navigation': true,
+        'navigationPosition':"left",
+        'navigationColor':['#fff'],
+        scrollBar: false,
+        responsiveWidth: 768,
+        scrollOverflow: true,
+        touchSensitivity: 15
+    });
+});
